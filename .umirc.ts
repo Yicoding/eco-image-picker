f29db12d91608c861bd40410efede0f2e90dcc8e
Yicoding <18063229116@163.com>
2021-11-14 16:12:02 +0800

    add

--- .umirc.ts
+++ .umirc.ts
@@ -25,7 +25,7 @@ const umiConfig = {
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs/antd-mobile-v3',
+  outputPath: 'site',
   // more config: https://d.umijs.org/config
   mode: 'doc',
   publicPath,
