8cf448a331008f97b107023a272ff677091c2550
Yicoding <18063229116@163.com>
2021-11-15 20:07:01 +0800

    feat: v1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { default as ImagePicker } from './ImagePicker';
-export { default as FilePicker } from './FilePicker';
 export { default as ImageView } from './ImageView';
-export { default as FileView } from './FileView';
