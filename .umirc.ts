a78faf97f69de086b1bd2e3616c1e3963535c291
Yicoding <18063229116@163.com>
2021-11-27 01:13:00 +0800

    feat: 删减配置

--- .umirc.ts
+++ .umirc.ts
@@ -33,17 +33,6 @@ const umiConfig = {
     type: 'none',
     exclude: [],
   },
-  // 配置具体含义见：https://github.com/umijs/umi-webpack-bundle-analyzer#options-for-plugin
-  analyze: {
-    analyzerMode: 'server',
-    analyzerPort: 8888,
-    openAnalyzer: true,
-    // generate stats file while ANALYZE_DUMP exist
-    generateStatsFile: false,
-    statsFilename: 'stats.json',
-    logLevel: 'info',
-    defaultSizes: 'parsed', // stat  // gzip
-  },
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
